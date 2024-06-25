04232f1ed08a19671a30520175ef212fa980029c
kanwalll <kanwalzahra22june@gmail.com>
2024-06-25 18:25:28 +0500

    commit

--- resources/views/frontend/infixlmstheme/pages/stepper.blade.php
+++ resources/views/frontend/infixlmstheme/pages/stepper.blade.php
@@ -81,57 +81,66 @@
         flex-direction: column;
         align-items: center;
     }
-@media only screen and (max-width: 576px){
-    .stepper_row {
+
+    @media only screen and (max-width: 576px) {
+        .stepper_row {
             flex-direction: column;
         }
-        .paddingy{
+
+        .paddingy {
             padding-top: 40px !important;
         }
+
         .slider {
             width: 100%;
             padding-left: 20px !important;
             padding-right: 20px !important;
             border-right: none !important;
             text-align: left !important;
-            margin-bottom: 20px; 
+            margin-bottom: 20px;
         }
+
         .slider h2 {
             padding-left: 0 !important;
         }
+
         .slider h2::before,
         .slider-right h2::before {
             display: none;
         }
+
         .slider-right {
             padding-left: 20px !important;
             padding-right: 20px !important;
         }
+
         .slider-right h2 {
             padding-left: 0 !important;
         }
+
         .stepper_row:not(:last-child) .slider::after {
-        content: "";
-        position: absolute;
-        left: 50%;
-        bottom: -60px;
-        transform: translateX(-50%);
-        width: 2px;
-        height: 60px;
-        background-color: #ccc;
+            content: "";
+            position: absolute;
+            left: 50%;
+            bottom: -60px;
+            transform: translateX(-50%);
+            width: 2px;
+            height: 60px;
+            background-color: #ccc;
+        }
+
+        .stepper_row:nth-child(2) .slider:not(:last-child)::after {
+            content: "";
+            position: absolute;
+            left: 50%;
+            bottom: -60px;
+            transform: translateX(-50%);
+            width: 2px;
+            height: 60px;
+            background-color: #ccc;
+        }
     }
 
-    .stepper_row:nth-child(2) .slider:not(:last-child)::after {
-        content: "";
-        position: absolute;
-        left: 50%;
-        bottom: -60px;
-        transform: translateX(-50%);
-        width: 2px;
-        height: 60px;
-        background-color: #ccc;   
-    }
-}
     @media only screen and (max-width: 767px) {
         .stepper_right {
             padding-left: 20px !important;
@@ -175,18 +184,36 @@
         <div class="row stepper_row px-xl-5 px-lg-4 px-3 row-padding">
             <div class="pl-0 slider d-flex flex-column align-items-md-end text-end paddingy" data-aos="fade-left"
                 data-aos-duration="1000">
-                <h2 class="custom_small_heading font-weight-bold">Step 1</h2>
-                <p class="mb-3">This is the first step This is the third step This is the first step This is the third
-                    step This is the third step This is the second step This is the third stepThis is the third step
-                    This is the third stepThis is the third step</p>
+                <h2 class="custom_small_heading font-weight-bold">The Mission & Story</h2>
+                <h4>Mission Statement:</h4>
+                <p class="mb-3">At Merkaii Xcellence Prep, our mission is to empower aspiring healthcare
+                    professionals through exceptional education and unwavering support, ensuring they
+                    achieve their fullest potential in the ever-evolving world of healthcare.</p>
+                <h4>Our Story:</h4>
+                <p class="mb-3">Our journey began in 2015 with a simple yet profound goal: to create a student-
+                    centered environment that fosters success in healthcare education. Over the years,
+                    we've grown from a small tutoring service in the basement into a comprehensive
+                    education hub, offering specialized review courses, personalized tutoring, and in-
+                    depth exam reviews. Our commitment to excellence and our passion for student
+                    success have driven us every step of the way.</p>
                 <div class="image"><img src="{{ asset('/public/uploads/images/footerimg/image1.jpg') }}"
                         class="w-100 h-100" style="object-fit: cover;"></div>
             </div>
             <div class="pr-0 slider slider-right stepper_right" data-aos="fade-right" data-aos-duration="1000">
-                <h2 class="custom_small_heading font-weight-bold">Step 2</h2>
-                <p class="mb-3">This is the second step This is the third stepThis is the first step This is the third
-                    step This is the third step This is the second step This is the third stepThis is the third stepThis
-                    is the third stepThis is the third stepThis is the third step</p>
+                <h2 class="custom_small_heading font-weight-bold">The Evolution & Transformation</h2>
+                <h4>Our Evolution:</h4>
+                <p class="mb-3">From our humble beginnings, we've continuously evolved to meet the needs of our
+                    students. What started as a modest family and friends tutoring service in the
+                    basement has transformed into a renowned institution known for its rigorous
+                    healthcare review courses and individualized tutoring programs. We've embraced
+                    new teaching methodologies, integrated cutting-edge technology, and expanded
+                    our curriculum to cover the diverse and dynamic field of healthcare.</p>
+                <h4>Our “Aha!” Moment:</h4>
+                <p class="mb-3">Our pivotal moment came when we realized the profound impact personalized
+                    education has on family and friends’ outcomes. Witnessing this we embarked on
+                    opening our services to others and observing students transform from struggling
+                    learners into confident, competent healthcare professionals. This has become our
+                    cornerstone approach in offering tailored education and one-on-one support.</p>
                 <div class="image"><img src="{{ asset('/public/uploads/images/footerimg/image1.jpg') }}"
                         class="w-100 h-100" style="object-fit: cover;"></div>
             </div>
@@ -194,18 +221,39 @@
         <div class="row stepper_row px-xl-5 px-lg-4 px-3 row-padding">
             <div class="pl-0 slider d-flex flex-column align-items-md-end text-end paddingy" data-aos="fade-right"
                 data-aos-duration="1000">
-                <h2 class="custom_small_heading font-weight-bold">Step 3</h2>
-                <p class="mb-3"> This is the third stepThis is the third step This is the first step This is the third
-                    stepThis is the third stepThis is the third step This is the second step This is the third stepThis
-                    is the third step This is the third step</p>
+                <h2 class="custom_small_heading font-weight-bold">Who We Serve</h2>
+                <h4>Our Audience:</h4>
+                <p class="mb-3"> Merkaii Xcellence Prep serves a diverse group of aspiring healthcare professionals,
+                    from nursing students to future doctors, pharmacists, and allied health practitioners.
+                    Our students come from various backgrounds, united by their dedication to
+                    healthcare and their desire to excel. We are here to guide them, support them, and
+                    celebrate their achievements along the way.</p>
                 <div class="image"><img src="{{ asset('/public/uploads/images/footerimg/image1.jpg') }}"
                         class="w-100 h-100" style="object-fit: cover;"></div>
             </div>
             <div class="pr-0 slider slider-right stepper_right" data-aos="fade-left" data-aos-duration="1000">
-                <h2 class="custom_small_heading font-weight-bold">Step 4</h2>
-                <p class="mb-3">This is the fourth stepThis is the fourth step This is the first step This is the
-                    third step This is the fourth step This is the second step This is the third stepThis is the third
-                    step This is the fourth stepThis is the fourth step</p>
+                <h2 class="custom_small_heading font-weight-bold">The Merkaii Values</h2>
+                <h4>Our Brand Values:</h4>
+                <p class="mb-3"><span class="font-weight-bold">Student-Centric:</span> Our students are at the heart
+                    of everything we do. Their success
+                    is our success.</p>
+                <p class="mb-3"><span class="font-weight-bold">Excellence:</span> We strive for excellence in all
+                    aspects of our educational offerings,
+                    ensuring high quality, comprehensive, and relevant content.</p>
+                <p class="mb-3"><span class="font-weight-bold">Innovation:</span> We embrace innovative teaching
+                    methods and technologies to
+                    enhance learning and keep pace with the evolving healthcare landscape.</p>
+                <p class="mb-3"><span class="font-weight-bold">Support:</span> We provide unwavering support, understanding
+                        that each student’s
+                        journey is unique and deserving of personalized attention.</p>
+                <p class="mb-3"><span class="font-weight-bold">Integrity:</span> We uphold the highest standards of
+                    integrity, fostering a trustworthy and
+                    respectful learning environment.</p>
+                <p class="mb-3"><span class="font-weight-bold">Teacher Well-Being:</span> We believe that happy
+                    teachers are the foundation of
+                    successful students. By taking exceptional care of our educators, we ensure they
+                    can focus wholeheartedly on their goals, bringing passion and dedication to every
+                    lesson.</p>
                 <div class="image"><img src="{{ asset('/public/uploads/images/footerimg/image1.jpg') }}"
                         class="w-100 h-100" style="object-fit: cover;"></div>
             </div>
